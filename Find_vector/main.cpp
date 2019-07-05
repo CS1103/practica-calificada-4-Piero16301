2fecb57534ae12960016977c07eb99ea5d40d5d9
Piero Morales <piero.morales@utec.edu.pe>
2019-07-05 13:05:56 -0500

    Versión final

--- Find_vector/main.cpp
+++ Find_vector/main.cpp
@@ -12,35 +12,23 @@ vector<TipoDato> lista;
 
 int step_i = 0;
 
+TipoDato maximo = 0;
+
 void* hilos(void* arg) {
     int core = step_i++;
 
     for (int i = core * lista.size() / 4; i < (core + 1) * lista.size() / 4; i++) {
-        int a = 5;
-        a = a + a;
+        for (int j = lista.size()-1; j > (lista.size()-1)-(lista.size()/4); j--) {
+            if (lista[j] > maximo) {
+                maximo = lista[j];
+            }
+            lista.pop_back();
+        }
     }
 }
 
 template <class T>
-T FindMax (vector<T> list) {
-    T max = 0;
-    for (int i = 0; i < list.size(); i++) {
-        if (list[i] > max) {
-            max = list[i];
-        }
-    }
-    return max;
-}
-
-int main () {
-    lista.push_back(5);
-    lista.push_back(10);
-    lista.push_back(25);
-    lista.push_back(93);
-    lista.push_back(53);
-    lista.push_back(92);
-    cout << "El maximo es: " << FindMax<TipoDato>(lista) << '\n';
-
+T FindMax () {
     pthread_t threads[MAX_THREAD];
 
     for (int i = 0; i < MAX_THREAD; i++) {
@@ -51,6 +39,30 @@ int main () {
     for (int j = 0; j < MAX_THREAD; j++) {
         pthread_join(threads[j], NULL);
     }
+
+    return maximo;
+}
+
+int main () {
+
+    lista.push_back(59);
+    lista.push_back(10);
+    lista.push_back(25);
+    lista.push_back(93);
+    lista.push_back(53);
+    lista.push_back(92);
+    lista.push_back(57);
+    lista.push_back(34);
+
+    cout << "Vector: ";
+
+    for (int i = 0; i < lista.size(); i++) {
+        cout << lista[i] << ((i == (lista.size()-1)) ? (' ') : (','));
+    }
+
+    cout << '\n';
+
+    cout << "El maximo es: " << FindMax<TipoDato>() << '\n';
 
     return 0;
 
